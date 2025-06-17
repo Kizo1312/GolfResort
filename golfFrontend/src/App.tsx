f52e30bda7d7d4ce921081b6fd11d73b7e4b858a
Ivan Vucetic <90158455+fivan14@users.noreply.github.com>
2025-06-17 19:22:51 +0200

    registration and login

--- golfFrontend/src/App.tsx
+++ golfFrontend/src/App.tsx
@@ -4,11 +4,13 @@ import Modal from './ui/Modal';
 import Navbar from './components/Navbar/Navbar'
 import { useModal } from "./ui/ModalContext";
 import Register from './components/Register/Register'
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   const [logRegister, setLogRegister] = useState('login')
   return (
     <>
+     <Toaster position="top-right" />
     <Navbar/>
       <Modal>
         {logRegister === 'login' ? <Login/> : <Register/>}
