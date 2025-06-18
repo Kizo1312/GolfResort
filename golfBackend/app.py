81c7980123bc244f4f0097661f6496451ff5dede
Kizo1312 <toma.ilcic@gmail.com>
2025-06-18 10:34:11 +0200

    konflikti

--- golfBackend/app.py
+++ golfBackend/app.py
@@ -8,32 +8,35 @@ from resources.services import blp as ServiceBlueprint
 
 from flask_cors import CORS
 
-from db import db  
+from db import db
 
 load_dotenv()
 
+def create_app(db_url=None):
+    app = Flask(__name__)
+    CORS(app)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///golf.db"
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+    app.config["API_TITLE"] = "Golf Resort API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 
-app = Flask(__name__)
-CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///golf.db"
 
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
-app.config["API_TITLE"] = "Golf Resort API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
+    db.init_app(app)
+    app.register_blueprint(UserBlueprint)
+    app.register_blueprint(ServiceBlueprint)
 
 
-db.init_app(app)
-app.register_blueprint(UserBlueprint)
-app.register_blueprint(ServiceBlueprint)
+    jwt = JWTManager(app)
+    api = Api(app)
 
-
-jwt = JWTManager(app)
-api = Api(app)
-
-if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+
+    return app
